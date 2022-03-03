1927fa76f772357825d2254e1e1c36ff980dcea0
Praveen Baratam <praveen.baratam@gmail.com>
2022-03-03 15:53:16 +0530

    Moved Strategies into individual files

---
+++ src/strategies/CandleTesterMult.py
@@ -0,0 +1,52 @@
+# coding: UTF-8
+import os
+import random
+
+import math
+import re
+import time
+
+import numpy
+from hyperopt import hp
+
+from src import logger, notify
+from src.indicators import highest, lowest, med_price, avg_price, typ_price, atr, MAX, sma, bbands, macd, adx, sar, cci, rsi, crossover, crossunder, \
+    last, rci, double_ema, ema, triple_ema, wma, ssma, hull, supertrend, rsx, donchian
+from src.exchange.bitmex.bitmex import BitMex
+from src.exchange.binance_futures.binance_futures import BinanceFutures
+from src.exchange.bitmex.bitmex_stub import BitMexStub
+from src.exchange.binance_futures.binance_futures_stub import BinanceFuturesStub
+from src.bot import Bot
+from src.gmail_sub import GmailSub
+
+# Candle tester for multiple timeframes
+class CandleTesterMult(Bot):
+    def __init__(self):
+        Bot.__init__(self, ['5m', '15m', '4h'])
+
+        self.ohlcv = {}
+
+        for i in self.bin_size:
+            self.ohlcv[i] = open(f"ohlcv_{i}.csv", "w")
+            self.ohlcv[i].write("time,open,high,low,close,volume\n") #header
+
+    # this is for parameter optimization in hyperopt mode
+    def options(self):
+        return {}
+
+    def strategy(self, action, open, close, high, low, volume):
+
+        if action not in ['5m', '15m', '4h']:
+            return
+
+        logger.info(f"---------------------------")
+        logger.info(f"Action: {action}")
+        logger.info(f"---------------------------")
+        logger.info(f"time: {self.exchange.timestamp}")
+        logger.info(f"open: {open[-1]}")
+        logger.info(f"high: {high[-1]}")
+        logger.info(f"low: {low[-1]}")
+        logger.info(f"close: {close[-1]}")
+        logger.info(f"volume: {volume[-1]}") 
+        logger.info(f"---------------------------")
+        self.ohlcv[action].write(f"{self.exchange.timestamp},{open[-1]},{high[-1]},{low[-1]},{close[-1]},{volume[-1]}\n")
---
+++ src/strategies/Doten.py
@@ -0,0 +1,42 @@
+# coding: UTF-8
+import os
+import random
+
+import math
+import re
+import time
+
+import numpy
+from hyperopt import hp
+
+from src import logger, notify
+from src.indicators import highest, lowest, med_price, avg_price, typ_price, atr, MAX, sma, bbands, macd, adx, sar, cci, rsi, crossover, crossunder, \
+    last, rci, double_ema, ema, triple_ema, wma, ssma, hull, supertrend, rsx, donchian
+from src.exchange.bitmex.bitmex import BitMex
+from src.exchange.binance_futures.binance_futures import BinanceFutures
+from src.exchange.bitmex.bitmex_stub import BitMexStub
+from src.exchange.binance_futures.binance_futures_stub import BinanceFuturesStub
+from src.bot import Bot
+from src.gmail_sub import GmailSub
+
+
+# Channel breakout strategy
+class Doten(Bot):
+    def __init__(self):
+        Bot.__init__(self, ['2h'])
+
+    def options(self):
+        return {
+            'length': hp.randint('length', 1, 30, 1),
+        }
+
+    def strategy(self, action, open, close, high, low, volume):
+        if action == '2h':
+            lot = self.exchange.get_lot()
+            length = self.input('length', int, 9)
+            up = last(highest(high, length))
+            dn = last(lowest(low, length))
+            self.exchange.plot('up', up, 'b')
+            self.exchange.plot('dn', dn, 'r')
+            self.exchange.entry("Long", True, round(lot / 20), stop=up)
+            self.exchange.entry("Short", False, round(lot / 20), stop=dn)
