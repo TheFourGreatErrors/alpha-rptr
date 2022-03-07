c314f0d8ab10453fc080f2adac64930e7f4b1dc5
Praveen Baratam <praveen.baratam@gmail.com>
2022-03-07 14:47:56 +0530

    Order Log enhancement for HTML Report, etc.

--- src/exchange/binance_futures/binance_futures_stub.py
+++ src/exchange/binance_futures/binance_futures_stub.py
@@ -58,7 +58,7 @@ class BinanceFuturesStub(BinanceFutures):
         self.isShortEntry = [False,False]        
 
         self.order_log = open("orders.csv", "w")
-        self.order_log.write("time,type,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
+        self.order_log.write("time,type,id,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
         
     def get_lot(self):
         """
@@ -120,7 +120,7 @@ class BinanceFuturesStub(BinanceFutures):
             return
         long = pos_size < 0 if True else False 
         ord_qty = abs(pos_size)
-        self.commit(id, long, ord_qty, self.get_market_price(), True, callback)
+        self.commit("Close", long, ord_qty, self.get_market_price(), True, callback)
     
     def close_all_at_price(self, price, callback=None):
         """
@@ -132,7 +132,7 @@ class BinanceFuturesStub(BinanceFutures):
             return
         long = pos_size < 0 if True else False 
         ord_qty = abs(pos_size)
-        self.commit(id, long, ord_qty, price, True, callback)
+        self.commit("Close", long, ord_qty, price, True, callback)
 
     def cancel(self, id):
         """
@@ -343,10 +343,8 @@ class BinanceFuturesStub(BinanceFutures):
 
             self.drawdown = (self.balance_ath - self.balance) / self.balance_ath * 100
 
-            # self.order_log.write("time,type,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
-            self.order_log.write(f"{self.timestamp},{'BUY' if long else 'SELL'},{price:.2f},{-self.position_size if abs(next_qty) else order_qty:.2f}, \
-                {self.position_avg_price:.2f},{0 if abs(next_qty) else self.position_size+order_qty:.2f}, \
-                {profit:.2f},{self.get_balance():.2f},{self.drawdown:.2f}\n")
+            # self.order_log.write("time,type,id,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
+            self.order_log.write(f"{self.timestamp},{'BUY' if long else 'SELL'},{id if next_qty == 0 else 'Reversal'},{price:.2f},{-self.position_size if abs(next_qty) else order_qty:.2f},{self.position_avg_price:.2f},{0 if abs(next_qty) else self.position_size+order_qty:.2f},{profit:.2f},{self.get_balance():.2f},{self.drawdown:.2f}\n")
             self.order_log.flush()
 
             self.position_size = self.get_position_size() + order_qty    
@@ -390,9 +388,8 @@ class BinanceFuturesStub(BinanceFutures):
             logger.info(f"//////// Current Position ////////////")
             logger.info(f"current position size: {next_qty} at avg. price: {self.position_avg_price}")
 
-            # self.order_log.write("time,type,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
-            self.order_log.write(f"{self.timestamp},{'BUY' if long else 'SELL'},{price:.2f},{next_qty if abs(order_qty) > abs(next_qty) else order_qty:.2f}, \
-                {self.position_avg_price:.2f},{self.position_size:.2f},{'-'},{self.get_balance():.2f},{self.drawdown:.2f}\n")
+            # self.order_log.write("time,type,id,price,quantity,av_price,position,pnl,balance,drawdown\n") #header
+            self.order_log.write(f"{self.timestamp},{'BUY' if long else 'SELL'},{id},{price:.2f},{next_qty if abs(order_qty) > abs(next_qty) else order_qty:.2f},{self.position_avg_price:.2f},{self.position_size:.2f},{'-'},{self.get_balance():.2f},{self.drawdown:.2f}\n")
             self.order_log.flush()
 
             self.set_trail_price(price)
