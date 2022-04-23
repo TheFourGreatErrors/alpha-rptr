fb3bc9b3ce5fea4c23ad12027afabc956996d91a
Praveen Baratam <praveen.baratam@gmail.com>
2022-04-23 13:07:35 +0530

    HTML Workbench V1

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 
 import base64
 import logging
-import os
+import os, tempfile
 import time
 import uuid
 from datetime import timedelta
@@ -49,6 +49,45 @@ allowed_range_minute_granularity = {
     "1d": ["1m", "1440T", 1440, 1440], "3d": ["1m", "4320T", 4320, 4320]
     # not support yet '1w', '2w', '1M'
 }
+# https://stackoverflow.com/questions/8299386/modifying-a-symlink-in-python/55742015#55742015
+def symlink(target, link_name, overwrite=False):
+    
+    '''
+    Create a symbolic link named link_name pointing to target.
+    If link_name exists then FileExistsError is raised, unless overwrite=True.
+    When trying to overwrite a directory, IsADirectoryError is raised.
+    '''
+
+    if not overwrite:
+        os.symlink(target, link_name)
+        return
+
+    # os.replace() may fail if files are on different filesystems
+    link_dir = os.path.dirname(link_name)
+
+    # Create link to target with temporary filename
+    while True:
+        temp_link_name = tempfile.mktemp(dir=link_dir)
+
+        # os.* functions mimic as closely as possible system functions
+        # The POSIX symlink() returns EEXIST if link_name already exists
+        # https://pubs.opengroup.org/onlinepubs/9699919799/functions/symlink.html
+        try:
+            os.symlink(target, temp_link_name)
+            break
+        except FileExistsError:
+            pass
+
+    # Replace link_name with temp_link_name
+    try:
+        # Pre-empt os.replace on a directory with a nicer message
+        if not os.path.islink(link_name) and os.path.isdir(link_name):
+            raise IsADirectoryError(f"Cannot symlink over existing directory: '{link_name}'")
+        os.replace(temp_link_name, link_name)
+    except:
+        if os.path.islink(temp_link_name):
+            os.remove(temp_link_name)
+        raise
 
 
 class FatalError(Exception):
