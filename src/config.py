63ce8bef9a901a44808ab960407f8dc97e83ca28
unknown <thefourgreaterrors@gmail.com>
2023-02-13 01:37:40 +0100

    Preparing config.py for Bybit implementation

--- src/config.py
+++ src/config.py
@@ -7,22 +7,40 @@ config = {
                     "binancetest1": {"API_KEY": "", "SECRET_KEY": ""},
                     "binancetest2": {"API_KEY": "", "SECRET_KEY": ""}
                     },
+    "bybit_keys": {
+                    "bybitaccount1": {"API_KEY": "", "SECRET_KEY": ""},
+                    "bybitaccount2": {"API_KEY": "", "SECRET_KEY": ""}
+                    },
+    "bybit_test_keys": {
+                    "bybittest1": {"API_KEY": "", "SECRET_KEY": ""},
+                    "bybittest2": {"API_KEY": "", "SECRET_KEY": ""}
+                    },
     "bitmex_keys": {
                     "bitmexaccount1": {"API_KEY": "", "SECRET_KEY": ""},
                     "bitmexaccount2": {"API_KEY": "", "SECRET_KEY": ""}
                     },
     "bitmex_test_keys": {
-                    "bitmextest1": {"API_KEY": "", "SECRET_KEY": ""},
+                    "bitmextest1":{"API_KEY": "", "SECRET_KEY": ""},
                     "bitmextest2": {"API_KEY": "", "SECRET_KEY": ""}
                     },
     "ftx_keys": {
                     "ftxaccount1": {"API_KEY": "", "SECRET_KEY": ""},
-                    "ftxaccount2": {"API_KEY": "", "SECRET_KEY": ""}
+                    "ftxaccount2": {"API_KEY": "", "SECRET_KEY": ""},                    
                 },
+    "bybit_keys": {
+                    "bybitaccount1": {"API_KEY": "", "SECRET_KEY": ""},
+                    "bybitaccount2": {"API_KEY": "", "SECRET_KEY": ""}
+                    },   
+    "bybit_test_keys": {
+                    "bybittest1": {"API_KEY": "", "SECRET_KEY": ""},
+                    "bybittest2": {"API_KEY": "", "SECRET_KEY": ""}
+                    },
     "line_apikey": {"API_KEY": ""},
     "discord_webhooks": {
 					"binanceaccount1": "",
-					"binanceaccount2": ""
+					"binanceaccount2": "",
+                                        "bybitaccount1": "",
+                                        "bybitaccount2": ""
                     },
     "healthchecks.io": {
                     "binanceaccount1": {
